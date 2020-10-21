a907af1f09874b35e0e8708061ee277e94cbc726
David Wisecarver <dwisecar@gmail.com>
2020-10-21 20:30:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,6 @@ require "pry"
 def nyc_pigeon_organizer(data)
   
   new_hash = data.each_with_object({}) do |key, value), final_array|
-    
     value.each do |inner_key, names|
       binding.pry
       names.each do |name|
